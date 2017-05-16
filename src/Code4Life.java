66f802c4809f3e070da9d0189ae0b9c167566848
loganitis899 <league@WTS20.attlocal.net>
2017-05-15 17:32:19 -0700

    wife
    grrrrr

--- src/Code4Life.java
+++ src/Code4Life.java
@@ -7,36 +7,39 @@ import javax.swing.JOptionPane;
 
 public class Code4Life {
 	public static void main(String[] args) {
-		
+
 		String hours = JOptionPane.showInputDialog("how many hours have you spent coding this week?");
 		int hourss = Integer.parseInt(hours);
-		 if(hourss>=5){
-			playBatmanTheme();
-			
-		}
-	
-		 else	if(hourss >=3){
+		if (hourss >= 3) {
 			JOptionPane.showMessageDialog(null, "you are a code ninja");
-		} else if(hourss<=2 && hourss>0){
+		}
+
+		if (hourss >= 5) {
+			playBatmanTheme();
+
+		}
+
+		else if (hourss <= 2 && hourss > 0) {
 			JOptionPane.showMessageDialog(null, "stop watching youtube and code");
 		}
-		
+
 	}
-	
-	
+
 	/*
 	 * Ask the user how many hours they spent coding this week.
-
+	 * 
 	 * 1. If it's 3 or more, tell them they're a Code Ninja.
 	 * 
-	 * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
+	 * 2. If it's less than 2, tell them to stop watching YouTube and write code
+	 * instead.
 	 * 
 	 * 3. If it's more than 5, play the Batman theme song.
 	 */
 
 	private static void playBatmanTheme() {
 		try {
-			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/League/Google Drive/league-sounds/batman.wav"));
+			AudioInputStream audioInputStream = AudioSystem
+					.getAudioInputStream(new File("/Users/League/Google Drive/league-sounds/batman.wav"));
 			Clip clip = AudioSystem.getClip();
 			clip.open(audioInputStream);
 			clip.start();
